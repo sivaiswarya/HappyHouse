ce0007269bb84bb74e10f448cd2229521a99c96b
sivaiswarya <sivaiswarya@gmail.com>
2015-04-02 22:11:44 -0500

    add all products list and few icons

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,29 @@
 class HomeController < ApplicationController
-  def index
-  	sql = "Select * from Categories"
-    @categories = ActiveRecord::Base.connection.execute(sql)
+  	def index
+
+  	@categories = home_categories
+
+    @products = home_products
+
+    @cat_products = cat_allproducts
+	end
+
+    private
+
+    def home_categories
+  		sql = "Select * from Categories"
+    	ActiveRecord::Base.connection.execute(sql)
+  	end
+
+  	def home_products
+    	sql = "Select * from Products order by category_id" 
+    	ActiveRecord::Base.connection.execute(sql)
+  	end
+
+  	def cat_allproducts
+  		sql="Select * from Categories where name = '#{params[:category_id]}'"
+  		ActiveRecord::Base.connection.execute(sql)
+  	end
+
   end
-end
+
