984c776fe08659bd19f6d58315dcd557de47b400
sivaiswarya <sivaiswarya@gmail.com>
2015-04-03 00:24:21 -0500

    view updates and add product show

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,29 +1,21 @@
 class HomeController < ApplicationController
-  	def index
+    def index
 
-  	@categories = home_categories
+      @categories = home_categories
 
-    @products = home_products
-
-    @cat_products = cat_allproducts
-	end
+      @products = home_products
+  end
 
     private
 
     def home_categories
-  		sql = "Select * from Categories"
-    	ActiveRecord::Base.connection.execute(sql)
-  	end
+      sql = "Select * from Categories"
+      ActiveRecord::Base.connection.execute(sql)
+    end
 
-  	def home_products
-    	sql = "Select * from Products order by category_id" 
-    	ActiveRecord::Base.connection.execute(sql)
-  	end
-
-  	def cat_allproducts
-  		sql="Select * from Categories where name = '#{params[:category_id]}'"
-  		ActiveRecord::Base.connection.execute(sql)
-  	end
-
+    def home_products
+      sql = "Select P.id, P.name, P.description, P.image, P.price, C.name, P.category_id from Products P, Categories C where P.category_id = C.id order by P.category_id"
+      ActiveRecord::Base.connection.execute(sql)
+    end
   end
 
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,6 +5,10 @@ class ProductsController < ApplicationController
     @products = get_products
   end
 
+  def show
+    @product = get_product
+  end
+
   private
 
   def get_categories
@@ -13,7 +17,12 @@ class ProductsController < ApplicationController
   end
 
   def get_products
-    sql = "Select P.name, P.description, P.image, P.price from Products P, Categories C where P.category_id = C.id and C.name = '#{params[:category_id]}'"
+    sql = "Select P.name, P.description, P.image, P.price, P.id from Products P, Categories C where P.category_id = C.id and C.name = '#{URI::decode(params[:category_id])}'"
     ActiveRecord::Base.connection.execute(sql)
+  end
+
+  def get_product
+    sql = "Select name, description, image, price from Products where id = '#{params[:id]}'"
+    ActiveRecord::Base.connection.exec_query(sql)
   end
 end
