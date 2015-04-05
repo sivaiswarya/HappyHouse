2e8170ef6b1397b34061c22e4482befdab43f722
sivaiswarya <sivaiswarya@gmail.com>
2015-04-04 20:21:13 -0500

    add users migrations and changes

--- Gemfile
+++ Gemfile
@@ -26,6 +26,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 gem "therubyracer"
 gem "less-rails"
 gem 'twitter-bootstrap-rails'
+gem 'bcrypt', '3.1.7'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150329013501) do
+ActiveRecord::Schema.define(version: 20150405011640) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name",       limit: 255
@@ -25,11 +25,20 @@ ActiveRecord::Schema.define(version: 20150329013501) do
     t.float    "price",       limit: 24
     t.text     "image",       limit: 65535
     t.integer  "category_id", limit: 4
-    t.datetime "created_at",                null: false
-    t.datetime "updated_at",                null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "products", ["category_id"], name: "index_products_on_category_id", using: :btree
+
+  create_table "users", force: :cascade do |t|
+    t.string   "first_name",      limit: 255
+    t.string   "last_name",       limit: 255
+    t.string   "email",           limit: 255
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+    t.string   "password_digest", limit: 255
+  end
 
   add_foreign_key "products", "categories"
 end
