06cac62dd0adae854553599a889023d286ab05c6
sivaiswarya <sivaiswarya@gmail.com>
2015-04-12 18:19:11 -0500

    Fix signup routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
     resources :products, category_id: /[A-Za-z0-9%\.]+?/
   end
   
-  get 'signup'    => 'users/new'
+  get 'signup'    => 'users#new'
   get 'login'     => 'sessions#new'
   post 'login'    => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
